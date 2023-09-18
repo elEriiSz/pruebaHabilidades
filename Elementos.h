55e74dc25a5707a85705f26b38d6002e76e758b8
Nyx_Android <elerii098@gmail.com>
2023-09-18 10:29:39 -0300

    Agregado los elementos

---
+++ Elementos.h
@@ -0,0 +1,43 @@
+#ifndef ELEMENTOS_H
+#define ELEMENTOS_H
+#include<string>
+
+struct elemento{
+	int pos;
+	std::string nombre;
+};
+
+const struct elemento FUEGO = {0, "Fuego"};
+const struct elemento ALUCINACIONES = {1, "Alucinaciones"};
+const struct elemento ELECTRICIDAD = {2, "Electricidad"};
+const struct elemento AGILIDAD = {3, "Agilidad"};
+const struct elemento LUZ = {4, "Luz"};
+const struct elemento VISTA = {5, "Potenciar Vista"};
+const struct elemento VELOCIDAD = {6, "Velocidad"};
+const struct elemento OIDO = {7, "Potenciar Oido"};
+const struct elemento AIRE = {8, "Aire"};
+const struct elemento OLFATO = {9, "Potenciar Olfato"};
+const struct elemento PENSAMIENTO = {10, "Potenciar Pensar"};
+const struct elemento MEMORIA = {11, "Potenciar Memoria"};
+const struct elemento LEER_MENTE = {12, "Leer Mente"};
+const struct elemento TIEMPO_MUNDIAL = {13, "Tiempo Mundial"};
+const struct elemento FUERZA = {14, "Fuerza"};
+const struct elemento SANGRE = {15, "Sangre"};
+const struct elemento AGUA = {16, "Agua"};
+const struct elemento GUSTO = {17, "Potenciar Gusto"};
+const struct elemento HUESOS = {18, "Huesos"};
+const struct elemento DUREZA = {19, "Dureza"};
+const struct elemento OSCURIDAD = {20, "Oscuridad"};
+const struct elemento SENTIMIENTOS = {21, "Ctrl Sentimientos"};
+const struct elemento RESISTENCIA = {22, "Resistencia"};
+const struct elemento IMAGINACION = {23, "Pot Imaginacion"};
+const struct elemento TIERRA = {24, "Tierra"};
+const struct elemento IDIOMA = {25, "Idioma"};
+const struct elemento TACTO = {26, "Pot Tacto"};
+const struct elemento PORTAL = {27, "Portal"};
+const struct elemento METAMORFOSIS = {28, "Metamorfosis"};
+const struct elemento TELEKINESIS = {29, "Telekinesis"};
+const struct elemento TIEMPO_LOCAL = {30, "Tiempo Local"};
+const struct elemento SUGESTION = {31, "Sugestion"};
+
+#endif
